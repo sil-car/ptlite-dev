839e4c41f7f52eb74cb3b78ca3dbe5898b77c816
Nate Marti <nate_marti@sil.org>
2024-09-24 18:34:09 +0100

    add zip download & streamline script

---
+++ get-latest-build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+drive_name="Nate SIL Google"
+builds_path="ParatextSharedBuilds"
+start_dir="$PWD"
+
+
+# Get latest build dir.
+release_dir=$(rclone lsd "${drive_name}":"${builds_path}" | tr -s ' ' | cut -d' ' -f6 | sort | tail -n1)
+
+# Get PT version number.
+ver=$(${release_dir##*_})
+
+# Download package.zip to folder named with version number.
+mkdir "$release_dir"
+cd "$release_dir"
+rclone copy "${drive_name}":"${builds_path}/${release_dir}/package.zip" . -P
+
+# Streamline package.zip.
+rm -rf package
+# Can't unzip to stdout b/c of ZIP format limitations; must extract to dir,
+# then recompress.
+unzip package.zip "package/Assets/*" "package/delete*" "package/LinuxArm64/*" "package/Terms/*"
+rm -f package.zip
+zip -r package.zip package
+rm -rf package
+cd "$start_dir"
